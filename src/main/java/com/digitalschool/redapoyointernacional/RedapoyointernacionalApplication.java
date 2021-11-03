10e6eed66baf7453f60d6b983497f8dc3a9ca5d4
Santiago <79651554+santiagoduque1998@users.noreply.github.com>
2021-11-02 21:56:04 -0500

    Archivos_Creados

---
+++ src/main/java/com/digitalschool/redapoyointernacional/RedapoyointernacionalApplication.java
@@ -0,0 +1,13 @@
+package com.digitalschool.redapoyointernacional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedapoyointernacionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedapoyointernacionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalschool/redapoyointernacional/RedapoyointernacionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalschool.redapoyointernacional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedapoyointernacionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
